fd0578ed51dbb56019309380567ff7cf9c2d68a1
Craig Fisk <craig.fisk@qa.com>
2022-05-26 01:01:18 +0100

    Updated home page and pre render

---
+++ content/pages/root/about.md
@@ -0,0 +1,7 @@
+Lead Engineer @ Qa 
+
+Building web component libraries and micro front ends using React.
+Teaching coding and 3D modelling @ Code Club.
+Building browser native video games using Typescript.
+
+Father, Husband, Developer, Gamer, Nerd
--- prerender-urls.js
+++ prerender-urls.js
@@ -4,13 +4,33 @@ const fs = require('fs');
 const parseMD = require('parse-md').default;
 
 const [blogs] = generateFileList(join(__dirname, 'content')).nodes;
+
+function generateBlogPageData(blog) {
+	let data;
+	if (blog.format === 'md') {
+		const { content } = parseMD(fs.readFileSync(join('content', 'blog', blog.id), 'utf-8'));
+		data = content;
+	} else {
+		data = fs.readFileSync(join('content', 'blog', blog.id), 'utf-8').replace(/---(.*(\r)?\n)*---/, '');
+	}
+	return {
+		url: `/blog/${blog.id}`,
+		seo: blog.details,
+		data: {
+			details: blog.details,
+			content: data
+		}
+	};
+}
+
 module.exports = () => {
 	const pages = [
 		{
 			url: '/',
 			seo: {
 				cover: '/assets/profile.jpg'
-			}
+			},
+			data: parseMD(fs.readFileSync(join('content', 'pages', 'root', 'about.md'), 'utf-8'))
 		},
 		{ url: '/contact/' },
 		{ url: '/contact/success' }
@@ -23,23 +43,27 @@ module.exports = () => {
 	});
 
 	// adding all blog pages
-	pages.push(...blogs.edges.map(blog => {
-		let data;
-		if (blog.format === 'md') {
-			const { content } = parseMD(fs.readFileSync(join('content', 'blog', blog.id), 'utf-8'));
-			data = content;
-		} else {
-			data = fs.readFileSync(join('content', 'blog', blog.id), 'utf-8').replace(/---(.*(\r)?\n)*---/, '');
-		}
-		return {
-			url: `/blog/${blog.id}`,
-			seo: blog.details,
-			data: {
-				details: blog.details,
-				content: data
-			}
-		};
-	}));
+	// pages.push(...blogs.edges.map(blog => {
+	// 	let data;
+	// 	if (blog.format === 'md') {
+	// 		const { content } = parseMD(fs.readFileSync(join('content', 'blog', blog.id), 'utf-8'));
+	// 		data = content;
+	// 	} else {
+	// 		data = fs.readFileSync(join('content', 'blog', blog.id), 'utf-8').replace(/---(.*(\r)?\n)*---/, '');
+	// 	}
+	// 	return {
+	// 		url: `/blog/${blog.id}`,
+	// 		seo: blog.details,
+	// 		data: {
+	// 			details: blog.details,
+	// 			content: data
+	// 		}
+	// 	};
+	// }));
+	const blogPages = [...blogs.edges.map(blog => generateBlogPageData(blog))];
+	pages.push(...blogPages);
+
+	pages[0].data['blog'] = blogPages.slice(0, 3);
 
 	return pages;
 };
